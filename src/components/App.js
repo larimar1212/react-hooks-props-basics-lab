315bc555efb04258634605afcecfbae9299bf6d7
larimar <larimarfondeur@gmail.com>
2022-08-08 21:11:36 -0400

    added

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,29 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
+const Footer = ({fontSize, fontColor, text}) => {
+  return(
+    <p>{text}</p>
+  )
+}
+
+
+
+const HeaderTag = ({textColor, tagText, backgroundColor}) => {
+  if(backgroundColor == 'red') return null
+  return(
+    <h1 style={{color: textColor, backgroundColor: backgroundColor }}>{tagText}</h1>
+  )
+}
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+     <HeaderTag textColor={'gray'} tagText={'fdsafds'}/>
+     <HeaderTag textColor={'yellow'} tagText={'Sandbox'} />
+     <HeaderTag backgroundColor={'red'} tagText={'Lari'} />
+     <Footer text={'this is the footer'} />
+     <Footer text={''} />
     </div>
   );
 }
